bc54118843ad3a99dedf8669410bef1e83a4081c
WestonBridson33 <wbridson97@gmail.com>
2021-12-23 08:13:21 -0600

    Created BaseProjectCard component and added a video for MonsterSlayer

---
+++ src/components/projectCards/BaseProjectCard.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-card
+    :color="color"
+    :elevation="elevation"
+    width="80%"
+    height="33em"
+    class="text-center secondary--text"
+    @mouseenter="hoverEnter"
+    @mouseleave="hoverExit"
+    @click="click"
+  >
+    <h3 class="text-h4 pt-4 pb-8">{{ cardTitle }}</h3>
+    <v-row class="pr-9">
+      <v-col class="pl-16 d-flex align-start justify-center">
+        <article style="max-width: 37em" class="pt-10">
+          {{ cardText }}
+        </article>
+      </v-col>
+      <v-col>
+        <video v-if="isVideo" width="540" height="380" autoplay muted>
+          <source :src="source" />
+        </video>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        source: {
+            type: String,
+            required: true,
+        },
+        isVideo: {
+            type: Boolean,
+            required: false,
+            default: true,
+        },
+        cardTitle: {
+            type: String,
+            required: true,
+        },
+        cardText: {
+            type: String,
+            required: true,
+        }
+    },
+  data: () => ({
+    elevation: 14,
+    color: "",
+  }),
+  methods: {
+    hoverEnter() {
+      this.elevation = 22;
+      this.color = "hover";
+    },
+    hoverExit() {
+      this.elevation = 14;
+      this.color = "primary darken-4";
+    },
+    click() {
+      /* @click gives a nice effect*/
+    },
+  },
+  mounted() {
+    this.hoverExit();
+    console.log(this.source, this.isVideo);
+    console.log(this.source == "../../Resources/Videos/MonsterSlayer.mp4")
+  },
+};
+</script>
+
+<style scoped>
+article {
+  max-width: 37em;
+  font-size: 1.2em;
+}
+</style>
--- src/components/projectCards/MonsterSlayerCard.vue
+++ src/components/projectCards/MonsterSlayerCard.vue
@@ -1,61 +1,22 @@
 <template>
-  <v-card
-    :color="color"
-    :elevation="elevation"
-    width="80%"
-    height="33em"
-    class="text-center secondary--text"
-    @mouseenter="hoverEnter"
-    @mouseleave="hoverExit"
-    @click="click"
-  >
-    <h3 class="text-h4 pt-4 pb-8">Monster Slayer</h3>
-    <v-row>
-      <v-col class=" pl-16 d-flex align-start justify-center">
-        <article style="max-width: 37em">
-          This project was part of a VueJS course on Udemy by Maximillian Schwarzmuller. It was a simple project pretty early in the course.
-          I really enjoyed working on it though, so I came back to it after finishing the course to expand on it. This project was mostly
-          for fun, but it also really helped me to solidify the more advanced aspects of vue like vuex.
-        </article>
-      </v-col>
-      <v-col>
-        <iframe
-          width="600"
-          height="380em;"
-          src="https://www.youtube.com/embed/6JUUDjMevVg?autoplay=1&mute=1&playlist=6JUUDjMevVg&loop=1&controls=0"
-          frameborder="2"
-        ></iframe>
-      </v-col>
-    </v-row>
-  </v-card>
+  <base-project-card 
+    :source="require('@/Resources/Videos/MonsterSlayer.mp4')"
+    cardTitle="Monster Slayer"
+    cardText="This project was part of a VueJS course on Udemy by Maximillian
+          Schwarzmuller. It was a simple project pretty early in the course. I
+          really enjoyed working on it though, so I came back to it after
+          finishing the course to expand on it. This project was mostly for fun,
+          but it also really helped me to solidify the more advanced aspects of
+          vue like vuex."
+   ></base-project-card>
 </template>
-
 <script>
+import BaseProjectCard from "./BaseProjectCard.vue";
+// import somkfe from "../../Resources/Videos/MonsterSlayer.mp4"
 export default {
-  data: () => ({
-    elevation: 14,
-    color: "",
-  }),
-  methods: {
-    hoverEnter() {
-      this.elevation = 22;
-      this.color = "hover";
-    },
-    hoverExit() {
-      this.elevation = 14;
-      this.color = "primary darken-4";
-    },
-    click() {/* @click gives a nice effect*/},
-  },
-  mounted() {
-    this.hoverExit();
-  },
-};
-</script>
-
-<style scoped>
-article {
-  max-width: 37em;
-  font-size: 1.2em;
+  components: {
+    BaseProjectCard,
+  }
 }
-</style>
+</script>
+  
