19d39ca4b41b86452e0180d3eb0fbedace33ac91
wbridson <wbridson@growmark.com>
2022-02-09 18:35:21 -0600

    Rearranged home page

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,9 +1,24 @@
 <template>
   <section>
-    <v-carousel height="52em" cycle class="d-flex justify-center primary darken-1">
-      <v-carousel-item v-for="(item, i) in images" :key="i" :src="item.src" :alt="item.name" style="width: 70em"></v-carousel-item>
-    </v-carousel>
-    <div class="secondary" style="min-height: 0.3em; min-width: 0.3em"></div>
+    <v-row>
+      <v-col cols="7" class="d-flex justify-center align-center secondary--text">
+        <div class="text-center">
+          <div>
+            <span><h2>ASPIRING</h2><v-img src="../../assets/logo.png" width="30"></v-img></span>
+          </div>
+          <div>
+            <h3>Web Developer</h3>
+          </div>
+          <div>
+            <h3>Game Developer</h3>
+          </div>
+        </div>
+      </v-col>
+      <v-col class="mt-0" style="padding-bottom: .33em; padding-top: .8em">
+        <img src="https://i.ibb.co/fQfZPg8/Profile-Pic.jpg" alt="Profile-Pic of Weston Bridson in Colorado" width="550" border="0" />
+      </v-col>
+    </v-row>
+    <div class="secondary mt-0" style="min-height: 0.3em; min-width: 0.3em"></div>
     <section id="portfolio" class="pt-6">
       <div class="heading secondary--text">
         <h1>Portfolio</h1>
@@ -44,7 +59,7 @@ import LetThereBeLightCard from "../projectCards/LetThereBeLightCard.vue";
 import MonsterSlayerCard from "../projectCards/MonsterSlayerCard.vue";
 import TownBuilderCard from "../projectCards/TownBuilderCard.vue";
 import CanteenCard from "../projectCards/CanteenCard.vue";
-import {mapGetters} from "vuex";
+import { mapGetters } from "vuex";
 export default {
   components: {
     LetThereBeLightCard,
@@ -52,53 +67,30 @@ export default {
     TownBuilderCard,
     CanteenCard,
   },
-// <img src="https://i.ibb.co/dPCSR9S/580-F0109-3-E59-42-DF-8221-1-F8-B00-C6-F088.jpg" alt="580-F0109-3-E59-42-DF-8221-1-F8-B00-C6-F088" border="0">
-// <img src="https://i.ibb.co/2NJwQLH/20210517-144852.jpg" alt="20210517-144852" border="0">
-// <img src="https://i.ibb.co/m6Pf3hB/20210517-144854.jpg" alt="20210517-144854" border="0">
-// <img src="https://i.ibb.co/nzMSDZ3/0927201553b-HDR.jpg" alt="0927201553b-HDR" border="0">
-// <img src="https://i.ibb.co/12GBhRF/Decorating.jpg" alt="Decorating" border="0">
-// <img src="https://i.ibb.co/6H53LFq/Decorating2.jpg" alt="Decorating2" border="0">
-// <img src="https://i.ibb.co/TkMjxGB/Dinner.jpg" alt="Dinner" border="0">
-// <img src="https://i.ibb.co/W3wdY3P/Holy-Weston.jpg" alt="Holy-Weston" border="0">
-// <img src="https://i.ibb.co/QXVRZ8H/IMG-2105.jpg" alt="IMG-2105" border="0">
-// <img src="https://i.ibb.co/dbm13TX/Mountain-Background.jpg" alt="Mountain-Background" border="0">
-// <img src="https://i.ibb.co/v1dStbP/Mountain-Squad.jpg" alt="Mountain-Squad" border="0">
-// <img src="https://i.ibb.co/zVWf3wv/Movie-Night.jpg" alt="Movie-Night" border="0">
-// <img src="https://i.ibb.co/s5Md55j/planting-Bushes.jpg" alt="planting-Bushes" border="0">
-// <img src="https://i.ibb.co/Rcsh2B3/planting-Bushes-2.jpg" alt="planting-Bushes-2" border="0">
-// <img src="https://i.ibb.co/dbm13TX/Mountain-Background.jpg" alt="Mountain-Background" border="0">
-  data: () => ({
-    images: [
-      {
-        name: "Planting Bushes at Newman Center",
-        src: "https://i.ibb.co/Rcsh2B3/planting-Bushes-2.jpg",
-      },
-      {
-        name: "Decorating for Christmas",
-        src: "https://i.ibb.co/12GBhRF/Decorating.jpg",
-      },
-      {
-        name: "Weston hiking in Colorado",
-        src: "https://i.ibb.co/dbm13TX/Mountain-Background.jpg",
-      },
-      {
-        name: "Service Team Picture",
-        src: "https://i.ibb.co/nzMSDZ3/0927201553b-HDR.jpg",
-      },
-      {
-        name: "Hiking to mountain top",
-        src: "https://i.ibb.co/v1dStbP/Mountain-Squad.jpg",
-      },
-    ],
-  }),
-  computed:{
-    ...mapGetters(["scrollPosY"])
+  // <img src="https://i.ibb.co/dPCSR9S/580-F0109-3-E59-42-DF-8221-1-F8-B00-C6-F088.jpg" alt="580-F0109-3-E59-42-DF-8221-1-F8-B00-C6-F088" border="0">
+  // <img src="https://i.ibb.co/2NJwQLH/20210517-144852.jpg" alt="20210517-144852" border="0">
+  // <img src="https://i.ibb.co/m6Pf3hB/20210517-144854.jpg" alt="20210517-144854" border="0">
+  // <img src="https://i.ibb.co/nzMSDZ3/0927201553b-HDR.jpg" alt="0927201553b-HDR" border="0">
+  // <img src="https://i.ibb.co/12GBhRF/Decorating.jpg" alt="Decorating" border="0">
+  // <img src="https://i.ibb.co/6H53LFq/Decorating2.jpg" alt="Decorating2" border="0">
+  // <img src="https://i.ibb.co/TkMjxGB/Dinner.jpg" alt="Dinner" border="0">
+  // <img src="https://i.ibb.co/W3wdY3P/Holy-Weston.jpg" alt="Holy-Weston" border="0">
+  // <img src="https://i.ibb.co/QXVRZ8H/IMG-2105.jpg" alt="IMG-2105" border="0">
+  // <img src="https://i.ibb.co/dbm13TX/Mountain-Background.jpg" alt="Mountain-Background" border="0">
+  // <img src="https://i.ibb.co/v1dStbP/Mountain-Squad.jpg" alt="Mountain-Squad" border="0">
+  // <img src="https://i.ibb.co/zVWf3wv/Movie-Night.jpg" alt="Movie-Night" border="0">
+  // <img src="https://i.ibb.co/s5Md55j/planting-Bushes.jpg" alt="planting-Bushes" border="0">
+  // <img src="https://i.ibb.co/Rcsh2B3/planting-Bushes-2.jpg" alt="planting-Bushes-2" border="0">
+  // <img src="https://i.ibb.co/dbm13TX/Mountain-Background.jpg" alt="Mountain-Background" border="0">
+  data: () => ({}),
+  computed: {
+    ...mapGetters(["scrollPosY"]),
   },
-  mounted(){
-    if(this.scrollPosY){
+  mounted() {
+    if (this.scrollPosY) {
       window.scrollTo(0, this.scrollPosY);
     }
-  }
+  },
 };
 </script>
 
